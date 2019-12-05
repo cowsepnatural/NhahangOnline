4cc2fada51e1419c4275814f3576a42306b7584c
= <=>
2019-12-06 05:09:45 +0700

    first commit

---
+++ yumi/yumi/Controllers/trangchuController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yumi.Models;
+
+namespace yumi.Controllers
+{
+    public class trangchuController : Controller
+    {
+        NhahangOnlineEntities db = new NhahangOnlineEntities();
+        // GET: trangchu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getDanhmuc()
+        {
+            var v = (from f in db.Danhmuc
+                     where f.hide == true
+                     orderby f.ID descending
+                     select f).Take(3);
+            return PartialView(v.ToList());
+        }
+        public ActionResult get_monan()
+        {
+            var v = (from f in db.Monan
+                     orderby f.ID descending
+                     select f).Take(4);
+            return PartialView(v.ToList());
+        }
+        public ActionResult get_thucuong()
+        {
+            var v = (from f in db.Thucuong
+                     orderby f.ID descending
+                     select f).Take(4);
+            return PartialView(v.ToList());
+        }
+    }
+}
---
+++ yumi/yumi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yumi.MvcApplication" Language="C#" %>
